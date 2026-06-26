4a86632b3841a1213769c15e56790bc0650c2b9a
Peter Atonga <peter.atonga@strathmore.edu>
2026-06-26 21:22:34 +0300

    feat: added Dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+// Dashboard page for Usaidizi Pap!
+
+
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <h1>Dashboard</h1>
+            <p>Welcome to Usaidizi Pap! Emergency First Aid Response System</p>
+        </div>
+    );
+};
+
+export default Dashboard;
