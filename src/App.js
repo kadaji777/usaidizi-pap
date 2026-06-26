9176262ed26b2f23fe3347a3d0ac967584cca1a3
Peter Atonga <peter.atonga@strathmore.edu>
2026-06-26 21:26:52 +0300

    feat: added App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// Usaidizi Pap! - Emergency First Aid Response System
+
+
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Usaidizi Pap!</h1>
+            <p>Emergency First Aid Response System</p>
+        </div>
+    );
+}
+
+export default App;
